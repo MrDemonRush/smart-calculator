e63c371d91088ef272333d6cf69527faf87a95f3
MDR <mrdemonrush@gmail.com>
2018-10-07 19:25:35 +0300

    smart-calcv1.0.0

--- src/index.js
+++ src/index.js
@@ -1,27 +1,96 @@
 class SmartCalculator {
+  
   constructor(initialValue) {
-    // your implementation
+    this.stackOp = [];
+    this.stackVal = [];
+    this.stackVal.push( initialValue );
   }
 
   add(number) {
-    // your implementation
+    this.stackOp.push( '+' );
+    this.stackVal.push( number );
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.stackOp.push( '-' );
+    this.stackVal.push( number );
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stackOp.push( '*' );
+    this.stackVal.push( number );
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stackOp.push( '/' );
+    this.stackVal.push( number );
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stackOp.push( '^' );
+    this.stackVal.push( number );
+    return this;
   }
+
+  valueOf(){
+
+    const arrOp = ['^', '*', '/', '+', '-'];
+
+    for( let i = 0; i < arrOp.length - 2; i++ ){
+
+        for( let j = this.stackOp.length - 1; j >= 0 ; j-- ){
+
+            if( this.stackOp[j] == arrOp[i] ){
+
+                switch( arrOp[i] ){
+                    
+                    case '^':{
+                        this.stackVal[j] = Math.pow( this.stackVal[j], this.stackVal[j + 1] );
+                        break;
+                    }
+
+                    case '*':{
+                        this.stackVal[j] = this.stackVal[j] * this.stackVal[j + 1];
+                        break;
+                    }
+
+                    case '/':{
+                        this.stackVal[j] = this.stackVal[j] / this.stackVal[j + 1];
+                        break;
+                    }
+
+                }
+
+                this.stackVal.splice( j + 1, 1 );
+                this.stackOp.splice( j, 1 );
+            }
+        }
+    }
+
+    while( this.stackOp.length ){
+
+        switch( this.stackOp[0 ]) {
+
+            case '+': {
+                this.stackVal[0] = this.stackVal[0] + this.stackVal[1];
+                break;
+            }
+            case '-': {
+                this.stackVal[0] = this.stackVal[0] - this.stackVal[1];
+                break;
+            }
+        }
+
+        this.stackVal.splice( 1, 1 );
+        this.stackOp.splice( 0, 1 );
+    }
+
+    return this.stackVal[0];
+}
 }
 
 module.exports = SmartCalculator;
